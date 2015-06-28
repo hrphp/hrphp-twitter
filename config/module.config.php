0ff5b24e34944ceb7de4536f5225b48ab822368d
tswayne <swaynetyler@gmail.com>
2015-06-28 12:36:00 -0400

    fix config oversight

--- config/module.config.php
+++ config/module.config.php
@@ -4,8 +4,9 @@ return [
     'service_manager' => [
         'invokables' => [],
         'factories' => [
-            'TwitterApiExchange' => 'HrPhp\Twitter\Adapter\Factory\TwitterApiExchangeFactory',
-            'TwitterApiExchangeAdapter' => 'HrPhp\Twitter\Adapter\Factory\TwitterApiExchangeAdapterFactory'
+            'TwitterApiExchange'        => 'HrPhp\Twitter\Adapter\Factory\TwitterApiExchangeFactory',
+            'TwitterApiExchangeAdapter' => 'HrPhp\Twitter\Adapter\Factory\TwitterApiExchangeAdapterFactory',
+            'HrPhpTwitterTimeline'      => 'HrPhp\Twitter\Factory\TimelineFactory'
         ]
     ]
 ];
